88889d35683d0031219050d5aec2b41efca9bb1e
Noah Greene <chevycam94@gmail.com>
2014-02-10 23:29:23 -0500

    Major cleanup
    Also corrected the name to match the package
    
    Change-Id: I3d1fc6aa1e11961226b58ab1963f13598dd3f3b0

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := AccountAndSyncSettings
+LOCAL_PACKAGE_NAME := AccountsAndSyncSettings
 LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
